cc1949120561de1caef25f3521fb3c54c85fa580
Jorgen14 <Jorgen14@users.noreply.github.com>
2023-05-04 12:14:38 -0700

    Organized everything

--- catkin_ws/src/missions/src/nav_channel.py
+++ catkin_ws/src/missions/src/nav_channel.py
@@ -7,36 +7,30 @@ from datetime import datetime, timedelta
 ROS_DEBUG = False
 
 if ROS_DEBUG:
-    rospy.init_node("TestNode", log_level=rospy.DEBUG)
+    rospy.init_node("NavChannel", log_level=rospy.DEBUG)
 else:
-    rospy.init_node("TestNode") 
+    rospy.init_node("NavChannel")  
+
+rate = rospy.Rate(1)   
 
 #MarthaVision = droneVision(DEBUG_CAM=False)
 
 MarthaCom = apCommunication()
 
-MarthaCom.arm(True)
-MarthaCom.change_mode("GUIDED")
-
 MarthaCom.clear_waypoints()
-
-rate = rospy.Rate(1)  
-wpTimer = datetime.now() + timedelta(seconds=10) 
-first = True
-second = False
-done = False
 
 if not MarthaCom.is_armed:
     MarthaCom.arm(True)
 
 while not rospy.is_shutdown():
     startTime = datetime.now()
-    try:
-        MarthaCom.change_mode("GUIDED")
-        MarthaCom.rotate_x_deg(90, 30)
-        #scriptTime = datetime.now() - startTime
-        #rospy.loginfo("Script time: " + str(scriptTime))
+    try:          
+        #MarthaVision.nav_channel_mission()
+        rospy.loginfo(str(MarthaCom.heading))
+        scriptTime = datetime.now() - startTime
+        rospy.loginfo("Script time: " + str(scriptTime))
         rate.sleep()
         
     except rospy.ROSInterruptException:
+        MarthaCom.RTL()
         break
---
+++ catkin_ws/src/missions/src/speed_gate.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3.8
+import rospy
+import time
+from martha_classes import droneVision, apCommunication
+from datetime import datetime, timedelta
+
+ROS_DEBUG = False
+
+if ROS_DEBUG:
+    rospy.init_node("SpeedGate", log_level=rospy.DEBUG)
+else:
+    rospy.init_node("SpeedGate")  
+
+rate = rospy.Rate(1)   
+
+MarthaVision = droneVision(DEBUG_CAM=False)
+
+MarthaCom = apCommunication()
+
+MarthaCom.clear_waypoints()
+
+if not MarthaCom.is_armed:
+    MarthaCom.arm(True)
+
+while not rospy.is_shutdown():
+    startTime = datetime.now()
+    try:          
+        MarthaVision.speed_gate_mission()
+        scriptTime = datetime.now() - startTime
+        rospy.loginfo("Script time: " + str(scriptTime))
+        rate.sleep()
+        
+    except rospy.ROSInterruptException:
+        MarthaCom.RTL()
+        break
