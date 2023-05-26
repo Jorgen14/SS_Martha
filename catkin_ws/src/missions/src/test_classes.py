74f0ede822bb5f380ed9c6957250af7bfda7f5ac
Jorgen14 <jorgerot@stud.ntnu.no>
2023-05-26 18:41:29 +0200

    Prep before autodrone

--- catkin_ws/src/missions/src/test_classes.py
+++ catkin_ws/src/missions/src/test_classes.py
@@ -11,7 +11,7 @@ if ROS_DEBUG:
 else:
     rospy.init_node("TestNode")  
 
-rate = rospy.Rate(5)   
+rate = rospy.Rate(1)   
 
 first = False
 second = False
@@ -19,29 +19,28 @@ third = False
 
 start_timer = datetime.now() + timedelta(seconds=125/4)
 
-MarthaVision = droneVision(DEBUG_CAM=False)
+#MarthaVision = droneVision(DEBUG_CAM=False)
 
-#MarthaCom = apCommunication()
+MarthaCom = apCommunication()
 
-#MarthaCom.clear_waypoints()
+MarthaCom.clear_waypoints()
 
-#if not MarthaCom.is_armed:
- #   MarthaCom.arm(True)
+if not MarthaCom.is_armed:
+    MarthaCom.arm(True)
 
-#MarthaCom.change_mode("GUIDED")
+MarthaCom.change_mode("GUIDED")
 
 while not rospy.is_shutdown():
     startTime = datetime.now()
     try:  
-        MarthaVision.detection_results()
+        MarthaCom.rotate_x_deg(30, 15)
+        break
         scriptTime = datetime.now() - startTime
         rospy.loginfo("Script time: " + str(scriptTime))
         rate.sleep()
         
     except rospy.ROSInterruptException:
-        #MarthaCom.stop()
+        MarthaCom.stop()
         break
 
 rospy.loginfo("Done!")
-
-
